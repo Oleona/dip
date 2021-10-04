b27304ad2cff1f69f46c810a4d04cb0494aadaee
Oleona <Ole-onna@mail.ru>
2021-10-04 19:35:24 +0300

    Получен List<Archive> data  с выборкой из базы, соответствующей текущим параметрам погода плюс минус 5 процентов и временем плюс минус три часа

--- test9/Controllers/MyarchiveController.cs
+++ test9/Controllers/MyarchiveController.cs
@@ -39,9 +39,9 @@ namespace test9.Controllers
             if (User.Identity.IsAuthenticated)
             {
                 this.Year = Year;
-               
-               // var archive = db.Archives.First(e => e.Year == Year);
-                
+               int DateHour = DateTime.Now.Hour;
+                // var archive = db.Archives.First(e => e.Year == Year);
+
                 HttpClient client = new HttpClient();
                 HttpResponseMessage response = await client.GetAsync("https://api.openweathermap.org/data/2.5/weather?q=%20Saint%20Petersburg,ru&appid=c986959d1fd2ecb4376e73e676671d28&units=metric");
                 //HttpContent content = response.Content;
@@ -56,8 +56,14 @@ namespace test9.Controllers
                 double temp = Double.Parse(Temp, CultureInfo.InvariantCulture);
                 double pressure = Double.Parse(Pressure, CultureInfo.InvariantCulture);
                  //var archive = db.Archives.FirstOrDefault(e => e.Year == Year);
-                var archive = db.Archives.Where(t =>(temp<= (t.Temperature*1.1))).FirstOrDefault() ;
+                var archive = db.Archives.Where(t =>(temp<= (t.Temperature*1.05))&&(humidity <=(t.Humidity*1.05))&& (pressure <= (t.Pressure * 1.05))).FirstOrDefault() ;
                  ViewBag.Archive = archive;
+                List<Archive> data = db.Archives.Where(t => 
+                (temp <= (t.Temperature * 1.05)&&temp>= (t.Temperature * 0.95)
+                && (humidity <= (t.Humidity * 1.05)&&humidity>=t.Humidity*0.95) 
+                && (pressure <= (t.Pressure * 1.05)&&pressure>=t.Pressure*0.95)
+                &&(DateHour<=t.Time+3)&&(DateHour>=t.Time-3)
+                )).ToList();
                 return View(archive);
             }
             else
